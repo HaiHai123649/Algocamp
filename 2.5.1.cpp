aabf4d15be089dce38697c25bdb814a233034e60
HaiHai123649 <3978723553@qq.com>
2026-02-05 19:10:08 +0800

    结构体排序-sort-cmp

--- 2.5.1.cpp
+++ 2.5.1.cpp
@@ -1,37 +1,31 @@
-#include<iostream>
-using  namespace std;
-int n;
-int m;
-int k[10005];
-int s[10005];
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+struct person{
+	int id;
+	int score;
+};
+bool cmp(person a,person b){
+	if(a.score != b.score)
+		return a.score>b.score;
+	return a.id<b.id;
+}
 int main(){
+	int n,m;
 	cin>>n>>m;
+	person p[5005];
+	int num=0;
 	for(int i=0;i<n;i++){
-		cin>>k[i]>>s[i];
+		cin>>p[i].id>>p[i].score;
 	}
-	 for(int i=0;i<n-1;i++){
-	 	for(int j=0;j<n-i-1;j++){
-	 		if(s[j]<s[j+1]){
-	 			swap(s[j],s[j+1]);
-				swap(k[j],k[j+1]); 
-			 }
-			 if(s[j] == s[j+1]){
-			 	if(k[j]>k[j+1]){
-			 		swap(k[j],k[j+1]); 
-				 }
-			 }
-		 }
-	 }
-	 int min = s[(int)(m*1.5)-1];
-	 int num = n;
-	 for(int i=0;i<n;i++){
-	 	if(s[i]<min){
-	 		num--;
-		 }
-	 } 
-	 cout<<s[(int)(m*1.5)-1]<<" "<<num<<endl;
-	 for(int i=0;i<num;i++){
-	 	cout<<k[i]<<" "<<s[i]<<endl;
-	 }
+	sort(p,p+n,cmp);
+	for(int i=0;i<n;i++){
+		if(p[i].score>=p[(int)(m*1.5)-1].score)
+			num++;
+	}
+	cout<<p[(int)(m*1.5)-1].score<<" "<<num<<endl;
+	for(int i=0;i<num;i++){
+		cout<<p[i].id<<" "<<p[i].score<<endl;
+	}
 	return 0;
 }
